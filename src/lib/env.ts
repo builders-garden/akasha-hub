0b392105ee329c5aef0b3f7bd80f0717ecb0afb9
Matteo Midena <matteo.midena@protonmail.com>
2025-01-13 17:20:10 +0100

    initial commit with drizzle, zod, env

---
+++ src/lib/env.ts
@@ -0,0 +1,22 @@
+import * as dotenv from "dotenv";
+import z from "zod";
+
+dotenv.config();
+
+const envSchema = z.object({
+  DATABASE_URL: z.string().url().trim().min(1),
+});
+
+const { data, success, error } = envSchema.safeParse(process.env);
+
+if (!success) {
+  console.error(
+    `An error has occurred while parsing environment variables:${error.errors.map(
+      (e) => ` ${e.path.join(".")} is ${e.message}`
+    )}`
+  );
+  process.exit(1);
+}
+
+export type EnvSchemaType = z.infer<typeof envSchema>;
+export const env = data;
